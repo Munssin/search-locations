aa2216e07982cdb87893ac49c73c7fe5d4da0760
Munssin <munssin@yandex.ua>
2017-05-03 14:37:28 +0300

    some cool style

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".b1").click(function(){
+        $("body").css({"background-color": "#e8eef7"});
+    });
+    $(".b2").click(function(){
+        $("body").css({"background-color": "#fff7f7"});
+    });
+    $(".b3").click(function(){
+        $("body").css({"background-color": "#fceaf4"});
+    });
+});
